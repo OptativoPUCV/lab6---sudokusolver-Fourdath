9cd83f1a7cf5965a2e9cd30f7cc8e973b3b1fdea
Replit user <>
2023-05-06 08:38:56 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -96,9 +96,7 @@ List* get_adj_nodes(Node* n){
                    new_node->sudo[i][j] = k;
                    if(is_valid(new_node)){
                        pushBack(list, new_node);
-                   //} else {
-                     //  free_node(new_node);
-                   }
+
                }
                return list;
            }
